89fea5fdee69200a8462a45124210265b7a86cf2
ApkaevIS <43430808+ApkaevIS@users.noreply.github.com>
2018-11-01 22:37:11 +0300

    Вторая лабораторная

--- 2.c
+++ 2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 int main()
 {
 
+int error;
 char c;
 float F, G, Y, a, x;
 
@@ -15,46 +15,55 @@ scanf ("%c", &c);
 switch (c)
 {
 	case '1':
-	printf ("Введите число a \n");
-scanf ("%f", &a);
-printf ("Введите число x \n");
-scanf ("%f", &x);
-	G = 5*(10*a*a-11*a*x+x*x)/24*a*a-49*a*x+15*x*x;
-	if (24*a*a-49*a*x+15*x*x == 0)
-	{
-		printf("Функция не может быть вычислена так как знаменатель не может быть равен 0\n");
-		exit(1);
-	}
-	printf("G=%f\n",G); break;
+	   printf ("Введите число a \n");
+       scanf ("%f", &a);
+       printf ("Введите число x \n");
+       scanf ("%f", &x);
+       if (24*a*a-49*a*x+15*x*x == 0)
+	        {
+	        error = 1; 	
+		    printf("Функция не может быть вычислена так как знаменатель не может быть равен 0\n");
+		    break;
+	        }
+	   G = 5*(10*a*a-11*a*x+x*x)/24*a*a-49*a*x+15*x*x;
+	   printf("%f\n",G);
+	break;
 
 	case '2':
-	printf ("Введите число a \n");
-scanf ("%f", &a);
-printf ("Введите число x \n");
-scanf ("%f", &x);
-	F = sinh(3*a*a+7*a*x+4*x*x);
-	printf("F=%f\n",F); break;
+	   printf ("Введите число a \n");
+       scanf ("%f", &a);
+       printf ("Введите число x \n");
+       scanf ("%f", &x);
+	   F = sinh(3*a*a+7*a*x+4*x*x);
+	   printf("F=%f\n",F);
+	break;
 
 	case '3':
-	printf ("Введите число a \n");
-scanf ("%f", &a);
-if(-0.1>a || a<0.1)
-{
-	printf("Число не пренадлжеит области определения функции \n");
-	exit(1);
-}
+	   printf ("Введите число a \n");
+       scanf ("%f", &a);
+       if (-0.1>=a && 0.1<=a)        
+            {
+            error = 2;
+	        printf("Число не пренадлжеит области определения функции \n");
+	        break;
+            }
 
-printf ("Введите число x \n");
-scanf ("%f", &x);
-if(-0.2>x || x<0.2)
-{
-	printf("Число не пренадлжеит области определения функции \n");
-	exit(1);
-}
-		Y = -atanh(30*a*a+37*a*x-4*x*x);
-	printf("Y=%f\n",Y); break;
+       printf ("Введите число x \n");
+       scanf ("%f", &x);
+       if (-0.25>=x && 0.25<=x)
+            {
+            error = 3;	
+	        printf("Число не пренадлжеит области определения функции \n");
+	        break;
+            }
+	   Y = -atanh(30*a*a+37*a*x-4*x*x);
+	   printf("Y=%f\n",Y);
+	break;
 
-	default: printf("Такой функции нету \n");
+	default:
+		printf("Такой функции нету \n");
+		error = 4;
+	break;
 }
-	return 0;
+	return error;
 }
--- 2.py
+++ 2.py
@@ -3,30 +3,34 @@ from math import atanh
 
 print ("Выберите функцию\n 1- G = 5*(10*a*a-11*a*x+x*x)/24*a*a-49*a*x+15*x*x \n 2- F = sinh(3*a*a+7*a*x+4*x*x\n 3- Y = -atanh(30*a*a+37*a*x-4*x*x)\n")
 c = int (input("Номер функции:"))
+
 if c == 1:
 	a = float (input ("a:"))
 	x = float (input ("x:"))
-	G = 5*(10*a*a-11*a*x+x*x)/24*a*a-49*a*x+15*x*x
-	if 24*a*a-49*a*x+15*x*x == 0:
+    if 24*a*a-49*a*x+15*x*x == 0:
 		print ("Функция не может быть вычислена так как знаменатель не может быть равен 0\n")
-		exit(0)	
+		exit(0)
+	G = 5*(10*a*a-11*a*x+x*x)/24*a*a-49*a*x+15*x*x
 	print ("G=""%2f" %G)
+
 elif c == 2:
 	a = float (input ("a:"))
 	x = float (input ("x:"))	
 	F = sinh(3*a*a+7*a*x+4*x*x)
 	print ("F=""%2f" %F)
+
 elif c == 3:
 	a = float (input ("a:"))
 	if -0.1>a or a>0.1:
 		print ("Число не пренадлжеит области определения функции \n")
 		exit(0)	
 	x = float (input ("x:"))
-	if -0.2>x or x>0.2:
+	if -0.25>x or x>0.25:
 		print ("Число не пренадлжеит области определения функции \n")
 		exit(0)	
 	Y = -atanh(30*a*a+37*a*x-4*x*x)
 	print ("Y=""%2f" %Y)
+	
 else:
 	print('Такой функции нету\n')
 	
